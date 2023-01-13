9e872a134b7bb5cb2582ce3afcc80a2f367c2f77
GUilherme <guilhermeclarodemorais@gmail.com>
2023-01-13 09:38:28 -0300

    project config [002]

--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
-const express = require("express");
+const express = require('express');
 
 const app = express();
 
-app.listen(3000, () => console.log("Server started at http://localhost:3000"));
+app.listen(3000, () => console.log('Server started at http://localhost:3000'));
 
-app.get("/", (request, response) => {
-  response.send("Hello World");
+app.get('/', (request, response) => {
+  response.send('Hello World');
 });
