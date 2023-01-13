33e8722ecf0b0cd52545105049769aed7bc333fd
GUilherme <guilhermeclarodemorais@gmail.com>
2023-01-13 09:27:24 -0300

    first commit [001]

---
+++ src/index.js
@@ -0,0 +1,9 @@
+const express = require("express");
+
+const app = express();
+
+app.listen(3000, () => console.log("Server started at http://localhost:3000"));
+
+app.get("/", (request, response) => {
+  response.send("Hello World");
+});
