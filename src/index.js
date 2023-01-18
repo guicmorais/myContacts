1087599c0a239e945bf340559d6dff2f2b762e00
GUilherme <guilhermeclarodemorais@gmail.com>
2023-01-18 19:43:12 -0300

    catching and handling errors

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 const express = require('express');
+require('express-async-errors');
 
 const routes = require('./routes');
 
@@ -6,6 +7,11 @@ const app = express();
 
 app.use(express.json());
 app.use(routes);
+// eslint-disable-next-line no-unused-vars
+app.use((error, request, response, next) => {
+  console.log(error);
+  response.sendStatus(500);
+});
 
 // eslint-disable-next-line no-console
 app.listen(3000, () => console.log('Server started at http://localhost:3000'));
